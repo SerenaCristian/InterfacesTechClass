24186fc38235d7de9c352731c54b91c7671595c7
SerenaCristian <devcristian02@gmail.com>
2024-10-15 22:07:50 -0300

    Criando Classes na Entidade

---
+++ InterfacesTechClass/Core/Entidades/Alimentos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesTechClass.Core.Entidades
+{
+    internal class Alimentos
+    {
+    }
+}
--- InterfacesTechClass/Program.cs
+++ InterfacesTechClass/Program.cs
@@ -6,7 +6,7 @@ namespace InterfacesTechClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
